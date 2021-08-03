68eafc2f47b46daa2ac30153e4af0cf67cbacca9
falahamin <falahamin@gmail.com>
2021-08-03 18:00:56 +0530

    made a change in propagate_clauses.py

--- propagate_clauses.py
+++ propagate_clauses.py
@@ -7,10 +7,10 @@ def propagate_clauses(model, k):
     for i in range(1, k+1):
         for c in frame_clauses[i]:
             n_c_prime = Not(convert_clauses(c, model.variables, model.next_state_variables))
-            temp = And(frame_variables[i], model.transition_formula, n_c_prime)
             solver.push()
-            solver.add(temp)
-            check_sat = solver.check()
+            solver.add(Implies(frame_variables[i], model.transition_formula))
+            solver.add(Implies(frame_variables[i],n_c_prime))
+            check_sat = solver.check(frame_variables[i])
             solver.pop()
             if check_sat == unsat:
                 add_clause_to_frames(i+1, c)
