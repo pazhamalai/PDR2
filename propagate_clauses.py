5dac95c7b4cd4cd6c1c0387e53b3985be8a6d85d
falahamin <falahamin@gmail.com>
2021-08-03 12:33:21 +0530

    added propagate_clauses.py

--- propagate_clauses.py
+++ propagate_clauses.py
@@ -1,12 +1,11 @@
 from z3 import *
-from FrameManager import frame_clauses,frame_variables,add_clause_to_frames
+from FrameManager import frame_clauses,frame_variables,add_clause_to_frames,solver
 from main import transition_formula,variables,next_state_variables
-temp_solver = Solver()
 
 
 def convert_clauses(clause,this_state,next_state):
     temp_clause = substitute(clause, (this_state[0],next_state[0]))
-    for i in range(1,len(this_state)):
+    for i in range(1, len(this_state)):
         temp_clause = substitute(temp_clause, (this_state[i],next_state[i]))
     return temp_clause
 
@@ -16,11 +15,11 @@ def propagate_clauses(k):
         for c in frame_clauses[i]:
             n_c_prime =Not(convert_clauses(c,variables,next_state_variables))
             temp = And(frame_variables[i],transition_formula,n_c_prime)
-            temp_solver.push()
-            temp_solver.add(temp)
-            check_sat = temp_solver.check()
-            temp_solver.pop()
-            if  check_sat == unsat:
+            solver.push()
+            solver.add(temp)
+            check_sat = solver.check()
+            solver.pop()
+            if check_sat == unsat:
                 add_clause_to_frames(i+1,c)
 
 
